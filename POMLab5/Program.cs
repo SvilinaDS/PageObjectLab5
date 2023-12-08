09f9a88afa98be7e4b72992768857d3a9fe36935
altjimmy <1295801@gmail.com>
2023-12-08 21:27:46 +0700

    program.cs

---
+++ POMLab5/Program.cs
@@ -0,0 +1,7 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver(); 
+driver.Url = "https://www.globalsqa.com/angularJs-protractor/SearchFilter/"; 
+Thread.Sleep(5000); 
+driver.Quit();
